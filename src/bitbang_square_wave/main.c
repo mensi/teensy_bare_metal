f742e02532c74ec1fea5e8dc7652434a19cf1c3e
Manuel Stocker <mensi@mensi.ch>
2016-01-02 17:31:31 +0000

    Add example to measure delay accuracy

---
+++ src/bitbang_square_wave/main.c
@@ -0,0 +1,20 @@
+#include "pins.h"
+#include "uart.h"
+#include "delay.h"
+
+#define OUTPUT_PIN 2
+
+int main(void) {
+  uart0_setup_default();
+  uart0_putline("\r\nbitbang_square_wave");
+
+  PIN_GPIO_PDDR( OUTPUT_PIN ) = (1 << PIN_PIN( OUTPUT_PIN ));
+  PIN_PORT_PCR( OUTPUT_PIN ) = PORT_PCR_MUX(1) | PORT_PCR_SRE_MASK | PORT_PCR_DSE_MASK;
+
+  while(1) {
+    pin_gpio_set_high(OUTPUT_PIN);
+    delay_ms_simple(1);
+    pin_gpio_set_low(OUTPUT_PIN);
+    delay_ms_simple(2);
+  }
+}
